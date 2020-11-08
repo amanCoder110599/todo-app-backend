bd42cb4cd458842a374ccc42dd1a37ebbee20767
amanCoder110599 <physicsaman110599@gmail.com>
2020-11-09 00:57:44 +0530

    Backend

---
+++ backend/build/precache-manifest.7668aa0111594da5e71bcdd513d48d82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a629a27a6aac296cdfdd82a398c646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8493ef8c8c1ccc854069",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "8493ef8c8c1ccc854069",
+    "url": "/static/js/2.ff4b0538.chunk.js"
+  },
+  {
+    "revision": "caa48d2021bbadb71425",
+    "url": "/static/js/main.c65d8383.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ backend/models/todo.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema(
+  {
+    ip: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    active: { type: Boolean, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = Todo;
